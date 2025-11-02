9291586fd143474f335ff7eafc6d4076795117f5
amirabbs <amirabbasnzri8@gmail.com>
2025-11-02 12:45:27 +0330

    changed message admin permission

--- neuralglass/main/admin.py
+++ neuralglass/main/admin.py
@@ -10,6 +10,13 @@ class MessageAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'subject')
     ordering = ('-created_at',)
     
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_add_permission(self, request):
+        return False 
+    def has_delete_permission(self, request, obj=None):
+        return True
+    
 @admin.register(SocialLink)
 class SocialLinksAdmin(admin.ModelAdmin):
     list_display = ('name', 'order', 'is_active')
